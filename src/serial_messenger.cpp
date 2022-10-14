e3ecf867f936cf61ad9e01d0d34a8ba3c4f75a8a
NestorDP <nestor-dp@hotmail.com>
2022-10-14 20:29:06 -0300

    create toolbar

--- src/serial_messenger.cpp
+++ src/serial_messenger.cpp
@@ -11,11 +11,16 @@ SerialMessenger::SerialMessenger(QWidget *parent)
     tabWidget->addTab(tab_messenger, "Messenger");
     tabWidget->addTab(tab_send_file, "Send/Receive file");
 
-    // QPixmap newpix("/usr/lib/x86_64-linux-gnu/qt5/examples/widgets/mainwindows/application/images/new.png");
+    QPixmap app_icon("icons/db9.png");
+    this->setWindowIcon(QIcon(app_icon));
 
-    QPixmap newpix("icons/db9.png");
-    this->setWindowIcon(QIcon(newpix));
-
-
+    QPixmap off_icon("icons/db9Off.png");
+    QPixmap save_icon("icons/save.png");
+    QPixmap new_icon("icons/new.png");
+    QPixmap open_icon("icons/open.png");
+    toolBar->addAction(QIcon(save_icon), "Save Chat");
+    toolBar->addAction(QIcon(new_icon), "New Chat");
+    toolBar->addAction(QIcon(open_icon), "Open Chat");
+    toolBar->addSeparator();
+    toolBar->addAction(QIcon(off_icon), "Connect");
 }
-
